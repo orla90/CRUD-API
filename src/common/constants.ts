ec50d989bb82b08d729c85fd16951255fa8ca36e
Olga Andrievich <oandrievich@fil-it.ru>
2024-10-17 10:00:28 +0300

    created http.server

---
+++ src/common/constants.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = 'api/users';
+export const CONTENT_TYPE_JSON = { "Content-Type": "application/json" };
+export const CONTENT_TYPE_HTML = { "Content-Type": "text/html" };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello, TypeScript with ts-node!");
+import * as dotenv from 'dotenv'
+import http, { IncomingMessage, ServerResponse } from 'http'
+import { resolve } from 'path'
+import { cwd } from 'process'
+import url from 'url';
+import { RequestType } from './common/enum/request-types.enum';
+import { BASE_URL, CONTENT_TYPE_JSON } from './common/constants';
+
+dotenv.config({ path: resolve(cwd(), '.env') });
+
+const server = http.createServer((request: IncomingMessage, response: ServerResponse) => {
+  const parsedUrl = url.parse(request.url || '', true);
+
+  if (request.method === RequestType.GET) {
+    handleGetRequest(request, response, parsedUrl);
+  } else if (request.method === RequestType.POST && parsedUrl.path === BASE_URL) {
+    handlePostRequest(request, response);
+  } else if (request.method === RequestType.PUT && parsedUrl.path?.startsWith(BASE_URL + '/')) {
+    handlePutRequest(request, response, parsedUrl);
+  } else if (request.method === RequestType.DELETE && parsedUrl.path?.startsWith(BASE_URL + '/')) {
+    handleDeleteRequest(request, response, parsedUrl);
+  } else {
+    sendResponse(response, 404, CONTENT_TYPE_JSON, { error: 'Method not allowed' });
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on ${ process.env.PORT }`);
+});
+
